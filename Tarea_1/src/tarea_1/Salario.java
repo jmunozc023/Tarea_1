de7a819358bbf355d5f8f29991d97ac0191ac839
jmunozc023 <jmunozc023@ulacit.ed.cr>
2023-05-21 20:57:19 -0600

    V0.4
    Creación de clase Salario

---
+++ Tarea_1/src/tarea_1/Salario.java
@@ -0,0 +1,10 @@
+public class Salario {
+    int codigo= 0;
+    float monto= 0;
+    int cantidad= 0;
+    public Salario (int codigo,float monto,int cantidad){
+        this.codigo= codigo;
+        this.monto= monto;
+        this.cantidad= cantidad;
+    }
+}
